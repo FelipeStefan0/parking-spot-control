c09d44d6fd270cda26906d160e2dd70e0d379d74
Felipe Stefano <felipe.stefano@parnamirim.rn.gov.br>
2023-04-10 11:23:40 -0300

    feat: criação dos components gerenciador(o component cadastro já estava criado, apenas foi mudado de pasta) e home(footer e header)

---
+++ parking-control-front/src/app/gerenciador/gerenciador.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GerenciadorComponent } from './gerenciador.component';
+
+describe('GerenciadorComponent', () => {
+  let component: GerenciadorComponent;
+  let fixture: ComponentFixture<GerenciadorComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ GerenciadorComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(GerenciadorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ parking-control-front/src/app/gerenciador/gerenciador.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gerenciador',
+  templateUrl: './gerenciador.component.html',
+  styleUrls: ['./gerenciador.component.scss']
+})
+export class GerenciadorComponent {
+
+}
