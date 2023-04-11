07d95df0a88e2f6474e23d3433de4f2d3a4089a7
Felipe Stefano <felipe.stefano@parnamirim.rn.gov.br>
2023-04-10 22:20:28 -0300

    style:estilização final da tabela e dos botões de excluir e editar

--- parking-control-front/src/app/app.module.ts
+++ parking-control-front/src/app/app.module.ts
@@ -6,8 +6,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CadastroComponent } from './gerenciador/cadastro/cadastro.component';
-import { FooterComponent } from './home/footer/footer.component';
-import { HeaderComponent } from './home/header/header.component';
 import { HomeComponent } from './home/home.component';
 import { GerenciadorComponent } from './gerenciador/gerenciador.component';
 
@@ -15,8 +13,6 @@ import { GerenciadorComponent } from './gerenciador/gerenciador.component';
   declarations: [
     AppComponent,
     CadastroComponent,
-    FooterComponent,
-    HeaderComponent,
     HomeComponent,
     GerenciadorComponent
   ],
