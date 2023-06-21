a58171b9a5746cd764bc0b6857b9c34ee48309d6
Felipe Stefano <felipe.stefano@parnamirim.rn.gov.br>
2023-06-21 15:26:46 -0300

    feat: importação do shared module no app module

--- parking-control-front/src/app/app.module.ts
+++ parking-control-front/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { HomeComponent } from './home/home.component';
 
 import { GerenciadorModule } from './gerenciador/gerenciador.module';
 import { HomeModule } from './home/home.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -24,6 +25,7 @@ import { HomeModule } from './home/home.module';
     HttpClientModule,
     GerenciadorModule,
     HomeModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
