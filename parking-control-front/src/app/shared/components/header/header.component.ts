aafe540bfaffcb57658d159acf6e5dc6e11d6403
Felipe Stefano <felipe.stefano@parnamirim.rn.gov.br>
2023-06-21 15:29:19 -0300

    feat: construção do header geral

--- parking-control-front/src/app/shared/components/header/header.component.ts
+++ parking-control-front/src/app/shared/components/header/header.component.ts
@@ -3,8 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss'],
-  standalone: true,
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
 
